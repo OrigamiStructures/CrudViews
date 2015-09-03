ae4c2a12be74fd48f6755c340863d53d1ba3e0c9
Don Drake <ddrake@dreamingmind.com>
2015-09-02 21:07:23 -0700

    add comments

--- src/View/Helper/CRUD/Decorator/FieldDecorator.php
+++ src/View/Helper/CRUD/Decorator/FieldDecorator.php
@@ -2,27 +2,69 @@
 namespace CrudViews\View\Helper\CRUD\Decorator;
 use CrudViews\View\Helper\CRUD\FieldOutputInterface;
 /**
- * FieldOutputDecorator
+ * FieldDecorator is the base class for Decorators
+ * 
+ * Establishes basic functionality, properties, and 
+ * the default behaviors for the Inferface method
  *
  * @author dondrake
  */
 class FieldDecorator implements FieldOutputInterface {
 	
+	/**
+	 * The decorated object
+	 *
+	 * @var object
+	 */
 	public $base;
 	
+	/**
+	 * The Helper class currently managing output
+	 * 
+	 * The helper also contains an entity property containing the 
+	 * entity currently being processed
+	 *
+	 * @var CrudHelper
+	 */
 	public $helper;
 	
-	public function __construct($object) {
+	/**
+	 * Decorate an object
+	 * 
+	 * The decorated objet gets stored in $this->base. 
+	 * Decorated objects always carry a helper and that gets 
+	 * stored in $this->helper.
+	 * 
+	 * @param FieldOutputInterface $object
+	 */
+	public function __construct(FieldOutputInterface $object) {
 		$this->base = $object;
 		if (!$this->helper) {
 			$this->helper = $this->base->helper;
 		}
 	}
 	
+	/**
+	 * Call the next output in the chain
+	 * 
+	 * DOES THIS TELL THE WHOLE STORY?
+	 * 
+	 * @param string $field The current column name being output
+	 * @param array $options options being sent to the next object in the sequence
+	 * @return string The product of the operation
+	 */
 	public function output($field, $options = array()) {
 		return $this->base->output($field, $options);
 	}
 	
+	/**
+	 * Does the entity the helper is holding have a uuid?
+	 * 
+	 * When building forms (and possibly other times) it is useful to uniquely 
+	 * id the entity so that input IDs and the like can truely be unique.
+	 * 
+	 * @return boolean
+	 */
 	public function hasUuid() {
 		return !is_null($this->helper->entity->_uuid);
 	}
--- src/View/Helper/CRUD/FieldOutputInterface.php
+++ src/View/Helper/CRUD/FieldOutputInterface.php
@@ -1,12 +1,8 @@
 <?php
 namespace CrudViews\View\Helper\CRUD;
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
 /**
+ * FieldOutputInterface sets the required methods for objects that can be Decorated
  *
  * @author dondrake
  */
