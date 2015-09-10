a94c20d25a8b6593aea8b80c823a07682163932c
Don Drake <ddrake@dreamingmind.com>
2015-09-09 22:17:48 -0700

    write override exception renderer

---
+++ src/Error/ExceptionRenderer.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace CrudViews\Error;
+
+use Cake\Error\ExceptionRenderer as BaseExceptionRenderer;
+
+/**
+ * Description of ExceptionRenderer
+ *
+ * @author dondrake
+ */
+class ExceptionRenderer extends BaseExceptionRenderer{
+	
+	/**
+	 * The exceptions belonging to the plugin
+	 *
+	 * @var array
+	 */
+	protected $_exceptions = [
+		'missingFieldSetupFile', 'missingFieldSetupException', 
+		'missingActionConfigException', 'missingConfigFileException'
+	];
+
+	/**
+	 * Make CrudView exception rendering templates work
+	 * 
+	 * @param \CrudViews\Error\Exception $exception
+	 * @param type $method
+	 * @param type $code
+	 * @return string
+	 */
+	protected function _template(Exception $exception, $method, $code)
+    {
+		parent::_template($exception, $method, $code);
+		if (in_array($this->template, $this->_exceptions)) {
+			$this->template = 'CrudViews.' . $this->template;
+		}
+		return $this->template;
+	}
+	
+}
