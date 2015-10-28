035e84e2495d69031837fe79045adce3fda43dd5
Don Drake <ddrake@dreamingmind.com>
2015-10-28 13:01:13 -0700

    fix attribute call

--- src/View/Helper/CRUD/Decorator/ResponsiveColumnDecorator.php
+++ src/View/Helper/CRUD/Decorator/ResponsiveColumnDecorator.php
@@ -11,7 +11,8 @@ use CrudViews\View\Helper\CRUD\Decorator\ColumnDecorator;
 class ResponsiveColumnDecorator extends ColumnDecorator {
 	
 	public function output($field, $options = array()) {
-		return $this->helper->Html->tag('div', $this->base->output($field, $options), $this->helper->CrudData->attributes($field, 'div'));
+//		debug($this->helper->CrudData->attributes($field, 'div'));
+		return $this->helper->Html->tag('div', $this->base->output($field, $options), $this->helper->CrudData->attributes("$field.div"));
 //		return '<td>' . $this->base->output($field, $options) . '</td>';
 	}
 
