65839c7dd5e62213549ab390d9384aa1b30ef2d6
Jason Tempestini <jason@curlymedia.com>
2015-10-28 14:28:54 -0700

    proper setup of attributes

--- src/View/Helper/CRUD/Decorator/ResponsiveHeadDecorator.php
+++ src/View/Helper/CRUD/Decorator/ResponsiveHeadDecorator.php
@@ -11,7 +11,7 @@ use CrudViews\View\Helper\CRUD\Decorator\ColumnDecorator;
 class ResponsiveHeadDecorator extends ColumnDecorator {
 	
 	public function output($field, $options = array()) {
-		return $this->helper->Html->tag('div', $this->helper->Paginator->sort($field), $this->helper->CrudData->attributes($field, 'div'));
+		return $this->helper->Html->tag('div', $this->helper->Paginator->sort($field), $this->helper->CrudData->attributes("$field.div"));
 	}
 
 }
